57343e6e5eb065b57ac5c951d70fcbdee47f49b6
Rejba <jppolona@o2.pl>
2022-12-14 01:38:39 +0100

    Add files via upload

---
+++ projekt_web/src/main/java/com/jsfcourse/projekt/projekt.java
@@ -0,0 +1,15 @@
+package com.jsfcourse.projekt;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class projekt {
+    
+}
